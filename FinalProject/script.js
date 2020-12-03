f83507c67c6331e9e72f6d2c77004fcfb8fdbebf
Steven Raimundo <sraimund@terpmail.umd.edu>
2020-12-02 20:45:38 -0500

    Start of Bulma and NavBar creation

---
+++ FinalProject/script.js
@@ -0,0 +1,7 @@
+//This will be for the mobile menu 
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active')
+});
