a0683c34059d5bcad56b6316192074c2166096fd
Steven Raimundo <sraimund@terpmail.umd.edu>
2020-12-02 22:08:52 -0500

    Package update

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,6 +1,6 @@
 //This will be for the mobile menu 
 const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
+const navbarMenu = document.querySelector('#mylist');
 
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active')
---
+++ FinalProject/server.js
@@ -0,0 +1,38 @@
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.route('/api')
+  .get((req, res) => {
+    console.log('GET request detected');
+    res.send(`Lab 5 for ${process.env.NAME}`);
+  })
+  .post(async(req, res) => {
+    console.log('POST request detected');
+    const data = await fetch('https://raw.githubusercontent.com/umdio/umdio-data/master/courses/data/202008.json');
+    const json = await data.json();
+    console.log('fetch request data', json);
+    res.send(json);
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
