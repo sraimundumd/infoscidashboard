e1734d43fe2939f929c5697aa81f70deb90bb63f
Steven Raimundo <sraimund@terpmail.umd.edu>
2020-12-13 19:46:40 -0500

    Structure Change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('FinalProject'));
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
